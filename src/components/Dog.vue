f41a065b02128ba523f384ee9c55959a2ad8fb96
Jaiver Andres <jaiverandres98@hotmail.com>
2020-03-24 17:37:50 -0500

    s

---
+++ src/components/Dog.vue
@@ -0,0 +1,25 @@
+<template>
+     <v-card>
+                  <v-img :src="dog.img" height="170px">
+                        
+                      </v-img>
+                  <v-card-title>
+                      <div>
+                          <h3>
+                              {{dog.name}}
+                          </h3>
+                          <p>{{dog.breed}}</p>
+                      </div>
+
+                  </v-card-title>
+</template>
+
+<script>
+export default {
+    props:{
+        dog:{
+            type:Object
+        }
+    }
+};
+</script>
